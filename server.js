d630cc16860c9df409866865b7b716f445e98714
Kevin2133 <kevin.bellocchio@gmail.com>
2022-08-20 22:22:56 +0200

    added template rendering

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-const { Server } = require("http");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//Setup Cors
 app.use(cors({
     origin: '*'
 }));
 
 
+//Setup static files
+app.use(express.static(__dirname + "/public"));
 
 
+//set views
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+
+//Handling routes
 app.get("/", (req, res) => {
-    res.header("Content-Type", "application/json");
-    res.send("Bella Elia");
-    res.end();
+    res.render("pages/index");
+});
+
+app.get("/posts", (req, res) => {
+    res.render("pages/template");
 });
 
 
 
 
 
-app.listen(port, () => console.log(`Listeniong on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
